6a5e82bc2668e840ea4e450262fb02e03e273fd2
tarie100 <tarie.school@gmail.com>
2024-05-18 03:19:55 -0700

    6. ... but why??

--- markdown2html.py
+++ markdown2html.py
@@ -1,57 +1,62 @@
 #!/usr/bin/python3
+"""
+This is a script to convert a Markdown file to HTML.
 
-'''
-This script takes two arguments: the first is a Markdown file name, and the second is an HTML output filename.
-'''
+Usage:
+    ./markdown2html.py [input_file] [output_file]
 
-import sys
-import os
+Arguments:
+    input_file: the name of the Markdown file to be converted
+    output_file: the name of the output HTML file
+
+Example:
+    ./markdown2html.py README.md README.html
+"""
+
+import argparse
+import pathlib
+import re
+
+
+def convert_md_to_html(input_file, output_file):
+    '''
+    Converts markdown file to HTML file
+    '''
+    # Read the contents of the input file
+    with open(input_file, encoding='utf-8') as f:
+        md_content = f.readlines()
+
+    html_content = []
+    for line in md_content:
+        # Check if the line is a heading
+        match = re.match(r'(#){1,6} (.*)', line)
+        if match:
+            # Get the level of the heading
+            h_level = len(match.group(1))
+            # Get the content of the heading
+            h_content = match.group(2)
+            # Append the HTML equivalent of the heading
+            html_content.append(f'<h{h_level}>{h_content}</h{h_level}>\n')
+        else:
+            html_content.append(line)
+
+    # Write the HTML content to the output file
+    with open(output_file, 'w', encoding='utf-8') as f:
+        f.writelines(html_content)
+
 
 if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        print('Usage: ./markdown2html.py README.md README.html', file=sys.stderr)
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser(description='Convert markdown to HTML')
+    parser.add_argument('input_file', help='path to input markdown file')
+    parser.add_argument('output_file', help='path to output HTML file')
+    args = parser.parse_args()
+
+    # Check if the input file exists
+    input_path = pathlib.Path(args.input_file)
+    if not input_path.is_file():
+        print(f'Missing {input_path}', file=sys.stderr)
         sys.exit(1)
 
-    if not os.path.isfile(sys.argv[1]):
-        print(f'Missing {sys.argv[1]}', file=sys.stderr)
-        sys.exit(1)
-
-    sym_map = {
-        '# ': ('<h1>', '</h1>\n'),
-        '## ': ('<h2>', '</h2>\n'),
-        '### ': ('<h3>', '</h3>\n'),
-        '#### ': ('<h4>', '</h4>\n'),
-        '##### ': ('<h5>', '</h5>\n'),
-        '###### ': ('<h6>', '</h6>\n')
-    }
-
-    ul = []
-    with open(sys.argv[1]) as rf:
-        read_lines = rf.readlines()
-        with open(sys.argv[2], 'a') as wf:
-            for line in read_lines:
-                if line.startswith('- '):
-                    ul.append(line)
-                if ul:
-                    i = 0
-                    while i < len(ul):
-                        if i == 0:
-                            html_line = ul[i].replace('- ', '<ul>\n<li>').replace(ul[i][-1], '</li>\n')
-                        else:
-                            html_line = ul[i].replace('- ', '<li>').replace('\n', '</li>\n')
-                        if i == len(ul) - 1:
-                            html_line = html_line.replace(html_line[-1], '\n</ul>\n')
-                        wf.write(html_line)
-                        i += 1
-                for k, v in sym_map.items():
-                    if line.startswith(k):
-                        html_line = line.replace(k, v[0]).replace('\n', v[1])
-                        break
-                if '**' in line:
-                    html_line = line.replace('**', '<b>').replace('\n', '</b>\n')
-                elif '__' in line:
-                    html_line = line.replace('__', '<em>').replace('\n', '</em>\n')
-                wf.write(html_line)
-
-    sys.exit(0)
-
+    # Convert the markdown file to HTML
+    convert_md_to_html(args.input_file, args.output_file)
